af72ed4ff1e8b52f5d4ca87dfe0f9298b8b9db6b
dream-jannis <me@jannis.space>
2025-09-15 13:23:57 +0800

    fehlende essenzielle Aufgabe

---
+++ -1_HelloWorld/hello_world.py
@@ -0,0 +1,41 @@
+# """
+# Das allererste Programm eines jeden angehenden Entwicklers.
+# Es ist nicht nur ein Test, ob dein Computer läuft, 
+# sondern auch ein philosophisches Statement:
+# "Ich habe verstanden, wie man Text auf den Bildschirm schreibt."
+# """
+
+# Der Befehl print() sorgt dafür, dass Python etwas auf den Bildschirm schreibt.
+# Hier z. B. den weltbekannten Text:
+print("Hello World!")
+
+
+#############################################Übungsaufgaben##############################################
+
+"""
+Aufgabe 1:
+Schreibe ein Programm, das "Hello World!" auf den Bildschirm ausgibt.
+(Tipp: Der Code steht schon oben, aber schreib ihn einfach nochmal.
+Wiederholung ist schließlich die Mutter des Programmierens.)
+"""
+
+"""
+Aufgabe 2:
+Ändere das Programm so, dass dein Name statt "World" ausgegeben wird.
+Beispiel:
+Hello Rainer Winkler!
+"""
+
+
+"""
+Aufgabe 3:
+Schreibe ein Programm, das zweimal hintereinander "Hello World!" ausgibt.
+Weil doppelt hält bekanntlich besser.
+"""
+
+
+"""
+Aufgabe 4:
+Schreibe ein Programm, das "Hello" und "World!" in zwei verschiedenen print()-Befehlen ausgibt.
+Das Ergebnis sollte in zwei Zeilen erscheinen.
+"""
